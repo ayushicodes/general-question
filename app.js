c7bd09a19936091a8aa8516499bc982ccb67bd53
ayushicodes <raiayushi12911@gmail.com>
2022-08-02 13:14:46 +0530

    functionality updated

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const btn = document.querySelectorAll('.question-btn')
+// const btn = document.querySelectorAll('.question-btn')
 
-btn.forEach(function (btn) {
-    btn.addEventListener("click", function (e) {
-        let showText = e.currentTarget.parentElement.parentElement
-        showText.classList.toggle("show-text")
+// btn.forEach(function (btn) {
+//     btn.addEventListener("click", function (e) {
+//         let showText = e.currentTarget.parentElement.parentElement
+//         showText.classList.toggle("show-text")
+//     })
+// })
+
+let questions = document.querySelectorAll(".question")
+
+questions.forEach(function (question) {
+    const btn = question.querySelector(".question-btn")
+    btn.addEventListener("click", function () {
+        questions.forEach(function (items) {
+            if (items !== question) {
+                items.classList.remove('show-text')
+            }
+        })
+        question.classList.toggle('show-text')
     })
 })
